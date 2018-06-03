f361c84afaed89683cae040d0d6bef46f455a1ae
robin2017 <robin.seu@foxmail.com>
2018-06-03 10:28:44 +0800

    first commit

---
+++ chap1/hello.js
@@ -0,0 +1,17 @@
+//1、用babel-node运行
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+});
+store.commit('increment');
+console.log(store.state.count);//1
+
